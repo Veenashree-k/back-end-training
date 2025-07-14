2a3fa46c7337a2b55bac2d8b0f70fe71c686998a
Veena.S <Veena.S@VH-PF4YZSDA>
2025-07-14 17:46:41 +0530

    testing project done

--- TestingProject/obj/Debug/net8.0/TestingProject.AssemblyInfo.cs
+++ TestingProject/obj/Debug/net8.0/TestingProject.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestingProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a2af130eecc72c59621a86346d4f8e3fd45e5949")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestingProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestingProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
